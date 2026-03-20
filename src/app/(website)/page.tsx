8f808b6d9c5086541722ea6ebc30762c7b65b5f2
LeomirDias <leomirdiasti@outlook.com>
2026-03-20 10:36:43 -0300

    chore: Remove unnecessary blank line before Location component for cleaner code structure

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -325,7 +325,6 @@ export default async function Home() {
         </Section>
 
         {/* Endereço */}
-
         <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-0">
           <Location />
         </div>
